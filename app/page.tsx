b7831410bcc8094354cd2fd5dd14974c9720ce77
Travis J <tfauvelle@gmail.com>
2023-11-16 15:14:38 -0500

    implemented auth with sign in and sign up pages

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,42 @@
 import { Button } from "@/components/ui/button";
+import { UserButton, auth } from "@clerk/nextjs/app-beta";
+import Link from "next/link";
+import { LogIn } from "lucide-react";
 
-export default function Home() {
-  return <Button>Click Me</Button>;
+export default async function Home() {
+  const { userId } = await auth();
+  const isAuth = !!userId;
+
+  return (
+    <div className="w-screen min-h-screen bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+
+          <div className="flex mt-2">
+            {isAuth && <Button>Go to Chats</Button>}
+          </div>
+          <p className="max-w-xl mt-2 text-lg text-slate-600">
+            Join the millions already searching and pushing the limits of how
+            they interact with PDFs thanks to AI powered chap
+          </p>
+          <div className="w-full mt-4">
+            {isAuth ? (
+              <h1>fileupload</h1>
+            ) : (
+              <Link href="/sign-in">
+                <Button>
+                  Login to get started
+                  <LogIn className="w-4 ml-2" />
+                </Button>
+              </Link>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
