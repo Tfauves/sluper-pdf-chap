bfe97006e71d7ab31a460d5c2d6bf6fc6f09a031
Travis J <tfauvelle@gmail.com>
2023-11-28 20:30:32 -0500

    schema and config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,6 +1,15 @@
-import type { Config } from "drizzle-kit";
+import { Config } from "drizzle-kit";
 
 export default {
+  dbCredentials: {
+    connectionString:
+      "postgresql://Tfauves:qbnT6hdDeHM1@ep-delicate-morning-65066635.us-east-2.aws.neon.tech/neondb?sslmode=require",
+  },
   driver: "pg",
-  schema: "./lib/db/schema",
-};
+  out: "./drizzle/migrations",
+  schema: "./lib/schema.ts",
+  strict: true,
+  verbose: true,
+} satisfies Config;
+
+// npx drizzle-kit push:pg
