c8d146be7063e9d8656c09206f1b6f899bccb145
Travis J <tfauvelle@gmail.com>
2023-11-28 20:58:28 -0500

    schema error

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,15 +1,13 @@
-import { Config } from "drizzle-kit";
+import type { Config } from "drizzle-kit";
+import * as dotenv from "dotenv";
+dotenv.config({ path: ".env" });
 
 export default {
-  dbCredentials: {
-    connectionString:
-      "postgresql://Tfauves:qbnT6hdDeHM1@ep-delicate-morning-65066635.us-east-2.aws.neon.tech/neondb?sslmode=require",
-  },
   driver: "pg",
-  out: "./drizzle/migrations",
-  schema: "./lib/schema.ts",
-  strict: true,
-  verbose: true,
+  schema: "./src/lib/db/schema.ts",
+  dbCredentials: {
+    connectionString: process.env.NEXT_PUBLIC_DATABASE_URL!,
+  },
 } satisfies Config;
 
 // npx drizzle-kit push:pg
